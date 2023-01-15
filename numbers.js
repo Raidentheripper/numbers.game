5008ea6b4ee5ac7d271e420b3d5ed0857c2092cc
Raidentheripper <you@example.com>
2023-01-15 14:08:20 +0200

    update

--- numbers.js
+++ numbers.js
@@ -1,62 +1,76 @@
+const slider = document.getElementBuId("my Range");
+const maxWindow = document.querySelector('.max');
+const minus = document.querySelector('.minus');
+const plus = document.querySelector('.plus');
+const diapazonAlert = document.querySelector('.diapazon');
+maxWindow.innerHTML = slider.value;
+diapAlert();
+
+slider.oniput = function() {
+    maxWindow.innerHTML = slider.value;
+    diapAlert();
+}
+
+const start = document.getElementById("start");
+start.addEventListener('click', numbersGame);
+minus.addEventListener('click', sliderminus);
+plus.addEventListener('click',sliderplus);
+
+function sliderminus () {
+ slider.value --;
+ maxWindow.innerHTML = slider.value;
+ diapAlert();
+}
+
+function sliderplus() {
+    slider.value++;
+    maxWindow.innerHTML = slider.value;
+    diapAlert ();
+}
+
+function diapAlert() {
+    diapazonAlert.innerHTML = `Потрібно вгадати число від 1 до ${slider.value}`;
+}
+
 function numbersGame() {
-    let diapozon = +prompt('Ведіть інтервал від 2 до ...');
+    let diapazom = Number(slider.value);
 
-    while (true) {
-        if (isNaN(diapozon)) {
-            alert('Потрібно ввести число!');
-            diapozon = +prompt('Ведіть інтервал від 2 до ...');
-        }
-        else {
-            break;    
-        }
-    }
-
-    alert(`Потрібно вгадати число в діапазоні від 1 до ${diapozon}`);
     let tries;
-    switch (diapozon) {
-        case 50:
-            tries = 5;
-            break;
-        case 100:
-            tries = 7;
-            break;
-        case 200:
-            tries = 10;
-            break;
-        default:
-            tries = 3;
+    // switch (diapazon) {
+    //    case 50:
+    //  tries =5;
+    //  break;
+    //    case 100:
+    //  tries = 7;
+    //  break;
+    //  case 200:
+    //  tries =10;
+    //  break;
+    //  default:
+    //  tries =3;
     }
-    const randomNumber = Math.floor(Math.random() * diapozon + 1);
+
+    let triesButton = document.getElementsByName('tries');
+    for(let i=0; i<triesButton.length; i++) {
+
+    }
+
+    const randomNumber = Math.floor(Math.random() * diapazon + 1);
     let number;
 
-    for(let i = 1; i <= tries; i++){
-        number = +prompt('Введіть ваш варінт');
-            if(number < randomNumber) {
-                alert('Загадане число більше за введене');
-                alert(`У вас залишилось ${tries - i} спроб`);
-            }
-            else if(number === randomNumber){
-                alert('Ви виграли! Супер!');
-                alert(`Ми вгадали за ${i} спроб `);
-                break;
-            }
-            else{
-                alert('Загадане число менше за введене');
-                alert(`У вас залишилось ${tries - i} спроб`);
-            }
+    for(let i = 1; i <= tries;i++) {
+    number = +prompt('Введіть ваш варіант');
+        if(number < randomNumber) {
+            alert('Загадане число більше за введене');
+            alert(`У вас залишилось ${tries - 1} спроб`);
         }
-}
-numbersGame();
-
-while (true) {
-    let Continue = prompt('Ще раз?', '1 - ТАК, 2 - НІ');
-    if (Continue == 1) {
-        numbersGame();
+        else if(number === randomNumber) {
+            alert('Ви виграли! Супер!');
+            alert(`Ми виграли за ${i} спроб`);
+            break;
+        }
+        else{
+            alert('Загадане число менше за введене');
+            alert(`У вас залишилось ${tries - i} спроб`)
+        }
     }
-    else if (Continue == 2) {
-        break;
-    }
-    else {
-        alert('Потрібно ввести 1 чи 2');
-    }
-}
